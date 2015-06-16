2b85e7b8391afcd6bee269ae65b95462dbfe07bc
Honza Dvorsky <honza@swiftkey.com>
2015-06-16 20:26:33 +0100

    trying to get both iOS and OS X playgrounds working

--- XcodeServerSDK/XcodeServerSDK.h
+++ XcodeServerSDK/XcodeServerSDK.h
@@ -6,14 +6,5 @@
 //  Copyright (c) 2015 Honza Dvorsky. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
-
-//! Project version number for XcodeServerSDK.
-FOUNDATION_EXPORT double XcodeServerSDKVersionNumber;
-
-//! Project version string for XcodeServerSDK.
-FOUNDATION_EXPORT const unsigned char XcodeServerSDKVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <XcodeServerSDK/PublicHeader.h>
 
 
